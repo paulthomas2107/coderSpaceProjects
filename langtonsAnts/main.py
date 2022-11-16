cfc135f3081d1efd15c9cd63f4f44219fb6e6da6
Paul Thomas <paulthomas2107@gmail.com>
2022-11-16 14:51:46 +0000

    More...

--- langtonsAnts/main.py
+++ langtonsAnts/main.py
@@ -1,4 +1,32 @@
 import pygame as pg
+from collections import deque
+
+
+class Ant:
+    def __init__(self, app, pos, color):
+        self.app = app
+        self.color = color
+        self.x, self.y = pos
+        self.increments = deque([(1, 0), (0, 1), (-1, 0), (0, -1)])
+
+    def run(self):
+        value = self.app.grid[self.y][self.x]
+        self.app.grid[self.y][self.x] = not value
+
+        SIZE = self.app.CELL_SIZE
+        rect = self.x * SIZE, self.y * SIZE, SIZE - 1, SIZE - 1
+
+        if value:
+            pg.draw.rect(self.app.screen, pg.Color('white'), rect)
+        else:
+            pg.draw.rect(self.app.screen, self.color, rect)
+
+        self.increments.rotate(1) if value else self.increments.rotate(-1)
+
+        dx, dy = self.increments[0]
+
+        self.x = (self.x + dx) % self.app.COLS
+        self.y = (self.y + dy) % self.app.ROWS
 
 
 class App:
@@ -11,8 +39,12 @@ class App:
         self.ROWS,  self.COLS = HEIGHT // CELL_SIZE, WIDTH // CELL_SIZE
         self.grid = [[0 for col in range(self.COLS)] for row in range(self.ROWS)]
 
+        self.ant = Ant(app=self, pos=[self.COLS // 2, self.ROWS // 2], color=pg.Color('orange'))
+
     def run(self):
         while True:
+            self.ant.run()
+
             [exit() for i in pg.event.get() if i.type == pg.QUIT]
             pg.display.flip()
             self.clock.tick()
