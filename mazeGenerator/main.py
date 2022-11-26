4fe42e37a9550899a1c0060d66c291a533d7f2c3
Paul Thomas <paulthomas2107@gmail.com>
2022-11-26 21:33:05 +0000

    More...

--- mazeGenerator/main.py
+++ mazeGenerator/main.py
@@ -6,6 +6,7 @@ TILE = 100
 cols, rows = WIDTH // TILE, HEIGHT // TILE
 
 pygame.init()
+pygame.display.set_caption('Maze Generator')
 sc = pygame.display.set_mode(RES)
 clock = pygame.time.Clock()
 
@@ -21,6 +22,10 @@ class Cell:
             'left': True
         }
         self.visited = False
+
+    def draw_current_cell(self):
+        x, y = self.x * TILE, self.y * TILE
+        pygame.draw.rect(sc, pygame.Color('saddlebrown'), (x + 2, y + 2, TILE - 2, TILE - 2))
 
     def draw(self):
         x, y = self.x * TILE, self.y * TILE
@@ -40,6 +45,46 @@ class Cell:
         if self.walls['left']:
             pygame.draw.line(sc, pygame.Color('darkorange'), (x, y + TILE), (x, y), 2)
 
+    @staticmethod
+    def check_cell(x, y):
+        find_index = lambda x, y: x + y * cols
+        if x < 0 or x > cols - 1 or y < 0 or y > rows - 1:
+            return False
+        return grid_cells[find_index(x, y)]
+
+    def check_neighbors(self):
+        neighbors = []
+        top = self.check_cell(self.x, self.y - 1)
+        right = self.check_cell(self.x + 1, self.y)
+        bottom = self.check_cell(self.x, self.y + 1)
+        left = self.check_cell(self.x - 1, self.y)
+        if top and not top.visited:
+            neighbors.append(top)
+        if right and not right.visited:
+            neighbors.append(right)
+        if bottom and not bottom.visited:
+            neighbors.append(bottom)
+        if left and not left.visited:
+            neighbors.append(left)
+        return choice(neighbors) if neighbors else False
+
+
+def remove_walls(current, next_one):
+    dx = current.x - next_one.x
+    if dx == 1:
+        current.walls['left'] = False
+        next_one.walls['right'] = False
+    elif dx == -1:
+        current.walls['right'] = False
+        next_one.walls['left'] = False
+    dy = current.y - next_one.y
+    if dy == 1:
+        current.walls['top'] = False
+        next_one.walls['bottom'] = False
+    elif dy == -1:
+        current.walls['bottom'] = False
+        next_one.walls['top'] = False
+
 
 grid_cells = [Cell(col, row) for row in range(rows) for col in range(cols)]
 current_cell = grid_cells[0]
@@ -52,7 +97,19 @@ while True:
         if event.type == pygame.QUIT:
             exit()
 
-    [cell.draw() for cell in grid_cells ]
+    [cell.draw() for cell in grid_cells]
+    current_cell.visited = True
+    current_cell.draw_current_cell()
+
+    next_cell = current_cell.check_neighbors()
+
+    if next_cell:
+        next_cell.visited = True
+        stack.append(current_cell)
+        remove_walls(current_cell, next_cell)
+        current_cell = next_cell
+    elif stack:
+        current_cell = stack.pop()
 
     pygame.display.flip()
     clock.tick(30)
